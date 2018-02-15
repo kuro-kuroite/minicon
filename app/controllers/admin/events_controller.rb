d50f23bca0c75f59a29f57c9aff6fe7ba5b0564e
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-02-15 09:28:19 +0000

    improve DRY by before_action

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,12 +1,14 @@
 class Admin::EventsController < ApplicationController
   layout 'admin'
   
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :event_params, only: [:create, :update]
+
   def index
     @events = Event.all
   end
   
   def show
-    @event = Event.find(params[:id])
   end
   
   def new
@@ -14,7 +16,6 @@ class Admin::EventsController < ApplicationController
   end
   
   def create
-    event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
     @event = Event.new(event_params)
     @event.save
     flash.notice = 'Event was successfully created.'
@@ -22,21 +23,26 @@ class Admin::EventsController < ApplicationController
   end
   
   def edit
-    @event = Event.find(params[:id])
   end
   
   def update
-    @event = Event.find(params[:id])
-    event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
     @event.update(event_params)
     flash.notice = 'Event was successfully updated.'
     redirect_to admin_event_path(@event)
   end
   
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
     flash.notice = "Event was successfully destroyed"
     redirect_to admin_events_path
   end
+  
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+    
+    def event_params
+      params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    end
 end
